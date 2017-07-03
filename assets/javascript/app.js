4c502b72a8e37595a65bfc66c498aace833b6f53
Terri Pannell <Terri Pannell>
2017-07-02 22:11:15 -0400

    Game can be run showing single questions at a time or all questions at once.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,24 +18,24 @@ var gameData = [
         	ans1:"The games afoot ", ans2:"Elementary, My dear Watson ", ans3:"When you have eliminated the impossible, whatever remains, however improbable, must be the truth", 
         	answer:"1", explanation:"Holmes does say 'Elementary' but never says 'My dear Watson'"},
 		{question:"How many Sherlockian societies are in existence?", ans1:"Between 100 and 400 ", 
-			ans2:"Between 400 and  800 ", ans3:"Over 800", answer:"2", explanation:"There are at least 900 know societies dedicated to all-things Homes."},
+			ans2:"Between 400 and  800 ", ans3:"Over 800", answer:"2", explanation:"There are at least 900 known societies dedicated to all-things Homes."},
 		{question:"What kind of doctor was Sherlock’s crimefighting cohort Dr. John Watson, originally?", 
 			ans1:"Internist ", ans2:"Rheumatist ", ans3:"Army surgeon ", answer:"2", explanation:"Watson is retired from the Army."},
 		{question:"The double-brimmed, houndstooth-patterned hat Sherlock famously wore (in the movies) is known as a what?", 
-			ans1:"Deerstalker ", ans2:"Monmouth ", ans3:"Cavalier ", answer:"0", explanation:"The deerstalker cap was originally worn on-stage by the actor that also added the pipe."},
+			ans1:"Deerstalker ", ans2:"Monmouth ", ans3:"Cavalier ", answer:"0", explanation:"The deerstalker cap was originally worn on-stage. He actually wore a Victorian cloth cap in Doyle's stories."},
 		{question:"What was the name of the street toughs who serve as informants and errand boys for Holmes and Watson?", 
-			ans1:"The Baker Street Irregulars", ans2:"The Scarlet Street Singulars", ans3:"The Criminal Kids", answer:"0", explanation: "The group appears numerous times to add Holmes."},
+			ans1:"The Baker Street Irregulars", ans2:"The Scarlet Street Singulars", ans3:"The Criminal Kids", answer:"0", explanation: "The group appears numerous times to aid Holmes."},
 		{question:"In the 1890 mystery “The Sign of the Four”, Sherlock employs what bit of evidence two years before a real-world police department would?", 
-			ans1:"Blood spatter analysis ", ans2:"Fingerprints ", ans3:"Handwriting analysis", answer:"1", explanation: "Holmes was analyzing fingerpints about 6 years before the first police department did so."},
+			ans1:"Blood spatter analysis ", ans2:"Fingerprints ", ans3:"Handwriting analysis", answer:"1", explanation: "Scotland Yard began analyzing fingerprints in 1901.  The first recorded use of fingerprints in a criminal investigation was 1893 in Argentina."},
 		{question:"What British actor starred in 14 Sherlock Holmes films between 1939 and 1946?", 
 			ans1:"Peter Finch", ans2:"Alec Guinness", ans3:"Basil Rathbone", answer:"2", explanation: "Basil Rathbone "},
 		{question:"Who directed the recent action-comedy Sherlock Holmes films?", 
 			ans1:"Guy Ritchie", ans2:"Quentin Tarintino", ans3:"Justing Lin", answer:"0", explanation: "These starred Robert Downey Jr."},
 		{question:"What happens in the 1893 short story—although only temporarily?", 
 			ans1:"Watson is committed to the London Sanitarium for the Criminally Insane ", ans2:"Sherlock and Watson become a vaudeville act ", 
-			ans3:"Sherlock dies", answer:"2", explanation: "After outrage ensued, Doyle revived Holmes later."},
+			ans3:"Sherlock dies", answer:"2", explanation: "He was killed off at the end of The Memoirs of Sherlock.  He came back to life in The Return of Sherlock Holmes."},
 		{question:"In the short story “His Last Bow”, Sherlock retires and takes up what hobby?", 
-			ans1:"Beekeeping ", ans2:"Making jam ", ans3:"Making soap ", answer:"0", explanation: "Why not? citation to be added to these"},
+			ans1:"Beekeeping ", ans2:"Making jam ", ans3:"Making soap ", answer:"0", explanation: "On a small farm in Sussex Downs."},
 			];
 
 
@@ -44,9 +44,11 @@ $('body').on('click', '#start', startGame);
 $('body').on('click', '#startOne', startGameOne);
 $('body').on('click', '#done', timedOut);
 $('body').on('click', '#restart', restartGame);
-$('body').on('click', '.option', nextQuestion);
+$('body').on('click', "input[name=radioOptions]", nextQuestion);
 
 function restartGame() {
+
+	resetGame();
 	if(bSingleGame){
 		startGameOne();
 	}
@@ -57,18 +59,16 @@ function restartGame() {
 
 function startGame() {
 
-	resetGame();
 
 	bSingleGame = false;
-	time = gameData.length * 15;
+	time = gameData.length * 15;	//15 seconds per question
 	showQuestions();}
 
 function startGameOne() {
 
-	resetGame();
 
 	bSingleGame = true;
-	time = 15;
+	time = 15;	//15 seconds per question
 	questCount = 0;
 	showQuestion();	
 }
@@ -118,7 +118,7 @@ function showQuestions() {
 				{
 					textString = gameData[i].ans3;
 				}
-				//options.append(textString);
+				
 				newDiv.append(options);
 				newDiv.append(textString);
 				newDiv.append("<br>");
@@ -140,7 +140,7 @@ function showQuestions() {
 }
 
 /* function: updateTimer
-	updates the timer on the display, changeing the color to yellow or red when appropriate
+	updates the timer on the display, changing the color to yellow or red when appropriate
 */
 function updateTimer(){
 	timerCount++;
@@ -231,7 +231,10 @@ function showQuestion(index) {
 	timerCount = 0;
 }
 
-
+/* nextQuestion
+	in single question mode, displays the next question and resets the timers and interval;
+	does nothing if all questions are displayed
+*/
 function nextQuestion() {
 
 	if(bSingleGame) {
@@ -241,9 +244,6 @@ function nextQuestion() {
 		$(".game").empty();
 
 			//Update statistics here
-
-
-
 			if(options[0].value === gameData[questCount-1].answer)
 			{
 				$(".game").addClass("question");
@@ -256,28 +256,24 @@ function nextQuestion() {
 				$(".question").append("Incorrect" + "<br>");
 				wrongAnswer++;
 			}
+			$(".question").append(gameData[questCount-1].explanation);
 
 
 			if(questCount < gameData.length)
 			{
 				questTimer = setTimeout(showQuestion, 5000);
-				//showQuestion(questCount);
 			}
 			else
 				questTimer = setTimeout(timedOut, 5000);
-				//timedOut();
 	}
 }
+
 /* timedOut
-	called when timer expires
+	called when timer expires; clears and shows next question if single question mode
+		or ends the game if all the questions are displayed at once
 */
 function timedOut(){
-
 //	val = $('input[name="radioOptions"]:checked', '.option').val();
-
-
-
-
 	if(questCount < gameData.length)
 	{
 		clearTimeout(questTimer);
@@ -291,16 +287,9 @@ function timedOut(){
 	}
 }
 
-
-/*function showTimer() {
-
-	$(".game").append("show timer");
-	$(".timer").text(questTimer);
-
-}*/
-
 /* endGame
-	displays correct and wrong answer count on the display, ends the timers
+	displays correct and wrong answer count on the display and ends the timers;
+		displays a restart button that will reset and begin another game
 	*/
 function endGame()
 {
